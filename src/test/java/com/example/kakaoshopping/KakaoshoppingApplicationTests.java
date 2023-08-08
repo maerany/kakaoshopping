6ef6318def7748bef511fad7ac14611d49dcc8dd
maerany <88430300+maerany@users.noreply.github.com>
2023-08-08 21:13:45 +0900

    01. Entity 만들기

---
+++ src/test/java/com/example/kakaoshopping/KakaoshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaoshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
